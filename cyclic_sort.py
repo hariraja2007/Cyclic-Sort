23c239074b045455091e098de5a61fd175c4fb81
HariRaja <99963016+hariraja2007@users.noreply.github.com>
2024-05-18 13:42:06 +0530

    Add files via upload

---
+++ cyclic_sort.py
@@ -0,0 +1,9 @@
+#Implementation of cyclic sort in python
+
+mylist=[4,2,3,5,1]
+
+for index in range(len(mylist)):
+    while mylist[index] != index+1:
+        correct_index=mylist[index]-1
+        mylist[index],mylist[correct_index]=mylist[correct_index],mylist[index]
+print(mylist)
